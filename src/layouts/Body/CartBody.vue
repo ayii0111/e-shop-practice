74dfaa1d85aed171ba8104405fdf60e3b2734cc7
ayii <ayii131420@gmail.com>
2026-05-15 20:07:37 +0800

    🚜 refactor: 購物車頁面的響應版本重作

--- src/layouts/Body/CartBody.vue
+++ src/layouts/Body/CartBody.vue
@@ -1,41 +1,32 @@
-<script setup>
-import { Button, Checkbox, Column, DataTable, Divider, InputNumber } from 'primevue'
+<script setup lang="ts">
+import { Button, Checkbox, Column, DataTable, DataView, Divider, InputNumber } from 'primevue'
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
 import { ProductService } from './LikeList'
 
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const isDesktop = breakpoints.greaterOrEqual('md') // 自動響應的 Ref<boolean>
 onMounted(() => {
   ProductService.getProductsSmall().then(data => (products.value = data))
 })
 
-const op = ref()
 const products = ref()
-const selectedProduct = ref()
-
-function displayProduct(event, product) {
-  op.value.hide()
-
-  if (selectedProduct.value?.id === product.id) {
-    selectedProduct.value = null
-  }
-  else {
-    selectedProduct.value = product
-
-    nextTick(() => {
-      op.value.show(event)
-    })
-  }
-}
-
-function toggle(event) {
-  op.value.toggle(event)
-}
 
 const quantity = ref(1)
 const inputNumberDt = {
-  root: { buttonWidth: '1.5rem' },
+  root: { buttonWidth: '2rem' },
 }
 const inputNumberPt = {
   pcInputText: {
-    root: 'py-1 px-2 min-w-1 w-10 text-center',
+    root: 'py-1 px-2 !min-w-1 w-10 text-center flex-none',
+  },
+}
+
+const inputNumberDt2 = {
+  root: { buttonWidth: '2.5rem' },
+}
+const inputNumberPt2 = {
+  pcInputText: {
+    root: 'py-1 px-2 min-w-1 w-20 text-center',
   },
 }
 
@@ -56,49 +47,62 @@ const buttonDt = {
     primaryHoverBackground: 'var(--danger-color)',
   },
 }
+const selectAll = ref(false)
+const checked = ref(false)
 </script>
 
 <template>
   <div class="mx-auto py-2 max-w-[1000px] min-h-[700px]">
-    <DataTable :pt="dataTablePT" :value="products" class="mx-4 border-t border-r border-l">
-      <Column field="check" class="p-1 border-r w-10 text-center">
-        <template #header>
-          <Checkbox v-model="selectAll" binary />
-        </template>
-        <template #body>
-          <Checkbox v-model="checked" binary />
-        </template>
-      </Column>
-      <Column header="商品圖" class="w-1/6">
-        <template #body="slotProps">
-          <div class="flex justify-center">
-            <img :src="`https://primefaces.org/cdn/primevue/images/product/${slotProps.data.image}`" :alt="slotProps.data.image" class="shadow-sm w-16" />
+    <DataView :value="products">
+      <template #list="slotProps">
+        <div class="flex flex-col">
+          <div v-for="(item, index) in slotProps.items" :key="index">
+            <div class="relative flex sm:flex-row flex-col sm:items-center sm:gap-4 sm:py-3 pt-4 pb-2" :class="{ 'border-t border-surface-200 dark:border-surface-700 ': index !== 0 }">
+              <Checkbox v-model="checked" class="sm:mx-4" binary />
+              <div data-label="商品圖片" class="relative sm:my-0 mt-1 mb-4 sm:w-32">
+                <img class="block xl:block mx-auto rounded w-full" :src="`https://primefaces.org/cdn/primevue/images/product/${item.image}`" :alt="item.name" />
+                <div class="absolute bg-black/70 rounded-border" style="left: 4px; top: 4px">
+                  <Tag :value="item.inventoryStatus"></Tag>
+                </div>
+              </div>
+              <div class="flex flex-1 justify-between sm:grid sm:grid-cols-[3fr_5fr]">
+                <div class="flex flex-row md:flex-col justify-between items-start gap-2">
+                  <div>
+                    <span class="font-medium text-surface-500 dark:text-surface-400 text-sm">{{ item.category }}</span>
+                    <div class="mt-2 font-medium text-lg">
+                      {{ item.name }}
+                    </div>
+                  </div>
+                </div>
+                <div class="sm:grid sm:grid-cols-[2fr_3fr]">
+                  <div data-label="數量" class="flex flex-col items-center mb-4">
+                    <InputNumber v-model="quantity" :dt="inputNumberDt" :pt="inputNumberPt" class="mb-2" inputId="horizontal-buttons" showButtons buttonLayout="horizontal" :step="1" :min="1" :max="99">
+                      <template #incrementbuttonicon>
+                        <span class="pi pi-plus" />
+                      </template>
+                      <template #decrementbuttonicon>
+                        <span class="pi pi-minus" />
+                      </template>
+                    </InputNumber>
+                    <span class="text-center"> 當前總數: 8</span>
+                  </div>
+                  <div data-label="價格" class="flex flex-col justify-center items-end sm:items-start sm:mr-8 sm:ml-8">
+                    <span class="mr-3 sm:mr-0 mb-2 font-semibold text-lg">${{ item.price }}</span>
+                    <span class="mr-3 sm:mr-0 text-gray-500 whitespace-nowrap">原價: 988</span>
+                    <!-- <div class="flex md:flex-row flex-row-reverse gap-2">
+                    </div> -->
+                  </div>
+                </div>
+                <Button icon="pi pi-times" variant="text" size="small" rounded severity="secondary" aria-label="Cancel" class="top-2 md:top-1 right-0 absolute">
+                  <template #loadingicon=""></template>
+                </Button>
+              </div>
+            </div>
           </div>
-        </template>
-      </Column>
-      <Column field="name" header="商品" class="w-1/6" bodyClass="whitespace-nowrap"></Column>
-      <Column field="price" header="售價" class="w-1/6">
-      </Column>
+        </div>
+      </template>
+    </DataView>
 
-      <Column header="數量" class="w-1/6">
-        <template #body>
-          <InputNumber v-model="quantity" :dt="inputNumberDt" :pt="inputNumberPt" class="" inputId="horizontal-buttons" showButtons buttonLayout="horizontal" :step="1" :min="1" :max="99">
-            <template #incrementbuttonicon>
-              <span class="pi pi-plus" />
-            </template>
-            <template #decrementbuttonicon>
-              <span class="pi pi-minus" />
-            </template>
-          </InputNumber>
-        </template>
-      </Column>
-      <Column header="" class=""></Column>
-      <Column header="小計" class="w-1/6">
-        <template #body="slotProps">
-          100
-        </template>
-      </Column>
-    </DataTable>
     <Divider />
 
     <div class="flex justify-between items-center m-4">
